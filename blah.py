cd3f22371a1ce9a1446fcd7a032d63d978347679
Patrick Swickard <swickape@hotmail.com>
2023-08-27 13:43:09 -0400

    blah improve

--- blah.py
+++ blah.py
@@ -73,10 +73,8 @@ for i in range(10,51):
   with open(file) as fd:
     lines = fd.read().splitlines()
     regex_hash['quote_list']['in_section'] = False
-    #inquotesection = False
     regex_hash['p_list']['in_section'] = False
     inpsection = False
-    inincludessection = False
     regex_hash['includes_list']['in_section'] = False
     for thisline in lines:
       thisfield = 'title'
@@ -93,51 +91,49 @@ for i in range(10,51):
           continue
       thisfield = 'quote_list'
       if regex_hash[thisfield]['value'] == None:
-        if regex_hash['quote_list']['in_section']:
+        if regex_hash[thisfield]['in_section']:
           this_end_regex = regex_hash[thisfield]['end_regex']
           thisquote = re.search(this_end_regex,thisline)
           if thisquote:
-            #inquotesection = False
-            regex_hash['quote_list']['in_section'] = False
+            regex_hash[thisfield]['in_section'] = False
             regex_hash[thisfield]['value'] = temp_quotelist
             continue
           else:
             temp_quotelist.append(thisline)
-        if not regex_hash['quote_list']['in_section']:
+        if not regex_hash[thisfield]['in_section']:
           this_start_regex = regex_hash[thisfield]['start_regex']
           thisquote = re.search(this_start_regex,thisline)
           if thisquote:
-            #inquotesection = True
-            regex_hash['quote_list']['in_section'] = True
+            regex_hash[thisfield]['in_section'] = True
             temp_quotelist = []
             continue
       thisfield = 'p_list'
       previousfield = 'quote_list'
       if regex_hash[thisfield]['value'] == None and not regex_hash[previousfield]['value'] == None:
-        if regex_hash['p_list']['in_section']:
+        if regex_hash[thisfield]['in_section']:
           this_end_regex = regex_hash[thisfield]['end_regex']
           thisp = re.search(this_end_regex,thisline)
           if thisp:
-            regex_hash['p_list']['in_section'] = False
+            regex_hash[thisfield]['in_section'] = False
             regex_hash[thisfield]['value'] = temp_plist
             continue
           else:
             temp_plist.append(thisline)
-        if not regex_hash['p_list']['in_section']:
+        if not regex_hash[thisfield]['in_section']:
           this_start_regex = regex_hash[thisfield]['start_regex']
           thisp = re.search(this_start_regex,thisline)
           if thisp:
-            regex_hash['p_list']['in_section'] = True
+            regex_hash[thisfield]['in_section'] = True
             temp_plist = []
             continue
       thisfield = 'includes_list'
       if regex_hash[thisfield]['value'] == None:
-        if regex_hash['includes_list']['in_section']:
+        if regex_hash[thisfield]['in_section']:
           this_end_regex = regex_hash[thisfield]['end_regex']
           thisp = re.search(this_end_regex,thisline)
           thisincludes = re.search(this_end_regex,thisline)
           if thisincludes:
-            regex_hash['includes_list']['in_section'] = False
+            regex_hash[thisfield]['in_section'] = False
             regex_hash[thisfield]['value'] = temp_includeslist
             continue
           else:
@@ -146,7 +142,7 @@ for i in range(10,51):
           this_start_regex = regex_hash[thisfield]['start_regex']
           thisincludessection = re.search(this_start_regex,thisline)
           if thisincludessection:
-            regex_hash['includes_list']['in_section'] = True
+            regex_hash[thisfield]['in_section'] = True
             temp_includeslist = []
             continue
       thisfield = 'sample_page_image'
