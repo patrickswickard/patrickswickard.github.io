b996bb66a04a856e38493659779b575f8b1d473f
Patrick Swickard <swickape@hotmail.com>
2023-08-31 11:19:19 -0400

    added sloppy get sections to extract most of webpage

--- get_sections.py
+++ get_sections.py
@@ -8,24 +8,28 @@ def init_field_hash():
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<title>[^<]*</title>"
   thisfield.end_regex = None
+  thisfield.inner_regex = r"<title>\s*(.*)\s*</title>"
   field_hash[thiskey] = thisfield
 
   thiskey = 'cover_image'
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<img[^>]*src=\"[^\"]*\""
   thisfield.end_regex = None
+  thisfield.inner_regex = r"<img[^>]*src=\"(.*)\""
   field_hash[thiskey] = thisfield
 
   thiskey = 'quote_list'
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<I>"
   thisfield.end_regex = r"</I>"
+  thisfield.inner_regex = None
   field_hash[thiskey] = thisfield
 
   thiskey = 'p_list'
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<P>"
   thisfield.end_regex = r"<P>"
+  thisfield.inner_regex = None
   thisfield.prerequisites = [field_hash['quote_list']]
   field_hash[thiskey] = thisfield
 
@@ -33,12 +37,14 @@ def init_field_hash():
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<UL>"
   thisfield.end_regex = r"</UL>"
+  thisfield.inner_regex = None
   field_hash[thiskey] = thisfield
 
   thiskey = 'sample_page_image'
   thisfield = FieldSection(thiskey)
   thisfield.start_regex = r"<img[^>]*src=\"[^\"]*\""
   thisfield.end_regex = None
+  thisfield.inner_regex = r"<img[^>]*src=\"(.*)\""
   field_hash[thiskey] = thisfield
 
   return field_hash
@@ -78,7 +84,50 @@ for i in range(1,51):
       if should_i_skip_rest_of_fields_for_this_line:
         continue
   print("*************")
+#  for thisfield in field_list:
+#    print(thisfield.fieldname)
+#    print(thisfield.value)
+#    if thisfield.inner_regex:
+#      print('Trying to grab the rest')
+#      textblob = ' '.join(thisfield.value)
+#      inner_value = re.search(thisfield.inner_regex,textblob)
+#      if inner_value:
+#        print("inner value: " + inner_value.group(1))
+#    print("*************")
+  field_hash = {}
   for thisfield in field_list:
-    print(thisfield.fieldname)
-    print(thisfield.value)
-    print("*************")
+    field_hash[thisfield.fieldname] = thisfield
+  title_lines = field_hash['title'].value
+  titleblob = ' '.join(title_lines)
+  title_regex = r"<title>\s*(.*)\s*</title>"
+  value = re.search(title_regex,titleblob).group(1)
+  print(value)
+  cover_image_lines = field_hash['cover_image'].value
+  cover_image_blob = ' '.join(cover_image_lines)
+  cover_image_regex = r"<img[^>]*src=\"(.*)\""
+  value = re.search(cover_image_regex,cover_image_blob).group(1)
+  print(value)
+  quote_list_lines = field_hash['quote_list'].value
+  value = []
+  for thisline in quote_list_lines:
+    linevalue_regex = r"^(.*?)\s*(?:<BR>\s*)?$"
+    linevalue = re.search(linevalue_regex,thisline).group(1)
+    value.append(linevalue)
+  print(value)
+  p_list_lines = field_hash['p_list'].value
+  p_list_blob = ' '.join(p_list_lines)
+  value = []
+  print(p_list_blob)
+  includes_list_lines = field_hash['includes_list'].value
+  includes_list_blob = ' '.join(includes_list_lines)
+  value = []
+  for thisline in includes_list_lines:
+    linevalue_regex = r"^\s*<LI>\s*<A[^>]*HREF=\"(.*?)\"[^>]*>\s*(.*)\s*</A>\s*$"
+    linevalue = [re.search(linevalue_regex,thisline).group(1),re.search(linevalue_regex,thisline).group(2)]
+    value.append(linevalue)
+  print(value)
+  sample_page_image_lines = field_hash['sample_page_image'].value
+  sample_page_image_blob = ' '.join(sample_page_image_lines)
+  sample_page_image_regex = r"<img[^>]*src=\"(.*)\""
+  value = re.search(sample_page_image_regex,sample_page_image_blob).group(1)
+  print(value)
