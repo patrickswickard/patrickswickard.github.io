1b082efc7ce7ea43589d797c8301e0a0a8d0cb8a
Patrick Swickard <swickape@hotmail.com>
2023-09-03 19:05:44 -0400

    read sections checkin

--- read_sections.py
+++ read_sections.py
@@ -16,6 +16,9 @@ for i in range(1,51):
   title = this_page_data['title']
   cover_image = this_page_data['cover_image']
   quote_list = this_page_data['quote_list']
+  p_list = this_page_data['p_list']
+  includes_list = this_page_data['includes_list']
+  sample_page_image = this_page_data['sample_page_image']
   output_file = open(filename, 'w')
   html_contents = """<!DOCTYPE html>
 <html>
@@ -38,5 +41,26 @@ for i in range(1,51):
     html_contents += thisline
     html_contents += "<BR>\n"
   html_contents += "</I>\n"
+  html_contents += "<P>\n"
+  for thisitem in p_list:
+    thisurl = thisitem['url']
+    thistext = thisitem['text']
+    html_contents += "<a href=\""  + thisurl + "\">" + thistext + "</a>. "
+  html_contents += "\n"
+  html_contents += "<P>\n"
+  html_contents += "Includes:\n"
+  html_contents += "<UL>\n"
+  for thisitem in includes_list:
+    thisurl = thisitem['url']
+    thistext = thisitem['text']
+    html_contents += "<LI><a href=\""  + thisurl + "\">" + thistext + "</A>. "
+  html_contents += "\n"
+  html_contents += "</UL>\n"
+  html_contents += "<P>\n"
+  html_contents += "Random sample page:\n"
+  html_contents += "<P>\n"
+  html_contents += "<img style=\"max-width:100%;height:auto;\" src=\"" + sample_page_image + "\">\n"
+  html_contents += "<P>\n"
+  html_contents += "</body>\n"
   output_file.write(html_contents)
   output_file.close()
